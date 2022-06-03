a4c9043cc017fd182a7f20d1752301b8e5dee709
spencer54025 <spencervp54025@gmail.com>
2022-06-03 11:42:10 -0600

    working fetch call

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Game from "./game";
+import Header from "./header";
+import Footer from "./footer";
 
 export default class App extends Component {
-  render() {
-    return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="app">
+				<Header />
+				<Game />
+				<Footer />
+			</div>
+		);
+	}
 }
